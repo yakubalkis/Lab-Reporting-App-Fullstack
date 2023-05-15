3ceffcd29a7b6acb9362befd06279749d3f693b0
yakubalkis <alkis.yakub@gmail.com>
2023-05-15 23:59:12 +0300

    Injected repo dependencies & added basic endpoint.

--- springboot-backend/src/main/java/com/labapp/controller/MainController.java
+++ springboot-backend/src/main/java/com/labapp/controller/MainController.java
@@ -1,5 +1,39 @@
 package com.labapp.controller;
 
+import com.labapp.entity.Report;
+import com.labapp.repository.LaborantRepository;
+import com.labapp.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1")
 public class MainController {
+
+    private LaborantRepository laborantRepository;
+    private ReportRepository reportRepository;
+
+    @Autowired
+    public MainController(LaborantRepository laborantRepository, ReportRepository reportRepository) {
+        this.laborantRepository = laborantRepository;
+        this.reportRepository = reportRepository;
+    }
+
+    @GetMapping("/reports")
+    public List<Report> getAllReports(){
+        return reportRepository.findAll();
+    }
+
+    
+
+
+
+
 
 }
