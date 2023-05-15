90945734c584b58bb190255b5048a96468e02b72
yakubalkis <alkis.yakub@gmail.com>
2023-05-15 16:57:23 +0300

    init

---
+++ springboot-backend/src/main/java/com/labapp/BeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.labapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringApplication.class, args);
+	}
+
+}
---
+++ springboot-backend/src/test/java/com/labapp/BeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
