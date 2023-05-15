9a1e7947339348541307a4c857ad641c6b5875fb
yakubalkis <alkis.yakub@gmail.com>
2023-05-15 19:28:02 +0300

    Created Header component.

---
+++ react-frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import "../style/Header.css"
+
+export default function Header(){
+
+
+    return(
+        <div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div className="navbar-brand">Laboratory Reporting App</div>
+                </nav>
+            </header>
+        </div>
+    )
+
+}
