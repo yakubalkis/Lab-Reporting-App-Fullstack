6154326b6cd15732cbbb9b3039b5780c5338c70a
yakubalkis <alkis.yakub@gmail.com>
2023-05-15 19:22:48 +0300

    Created basic structure with basic routing.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,20 @@
-
+import { Routes, Route } from 'react-router';
 import './App.css';
+import Header from './components/Header';
+import ListReports from './components/ListReports';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div className="container">
+        <Header/>
+        <div className="container">
+            <Routes>
+                <Route path="/" element={<ListReports />} />
+            </Routes>
+        </div>
+
+      </div>
     </div>
   );
 }
