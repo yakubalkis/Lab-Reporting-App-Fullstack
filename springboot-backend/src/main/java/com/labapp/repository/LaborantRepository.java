3446be0718fc5f3638ef7d8e1666d901987576a2
yakubalkis <alkis.yakub@gmail.com>
2023-05-16 01:29:23 +0300

    Added custom query method.

--- springboot-backend/src/main/java/com/labapp/repository/LaborantRepository.java
+++ springboot-backend/src/main/java/com/labapp/repository/LaborantRepository.java
@@ -2,7 +2,10 @@ package com.labapp.repository;
 
 import com.labapp.entity.Laborant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LaborantRepository extends JpaRepository<Laborant, Integer> {
 
+    @Query("SELECT lab FROM Laborant lab WHERE lab.hospitalIdNo = ?1")
+    Laborant findLaborantByHospitalIdNo(String hospitalIdNo);
 }
