28133c9fe2fb69001335ec9688ea18febdae79dd
yakubalkis <alkis.yakub@gmail.com>
2023-05-16 01:28:20 +0300

    Added mappings to get report and create report.

--- springboot-backend/src/main/java/com/labapp/controller/MainController.java
+++ springboot-backend/src/main/java/com/labapp/controller/MainController.java
@@ -1,13 +1,13 @@
 package com.labapp.controller;
 
+import com.labapp.entity.Laborant;
 import com.labapp.entity.Report;
 import com.labapp.repository.LaborantRepository;
 import com.labapp.repository.ReportRepository;
+import com.labapp.service.LaborantService;
+import com.labapp.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,23 +16,40 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class MainController {
 
-    private LaborantRepository laborantRepository;
-    private ReportRepository reportRepository;
+    private LaborantService laborantService;
+    private ReportService reportService;
 
     @Autowired
-    public MainController(LaborantRepository laborantRepository, ReportRepository reportRepository) {
-        this.laborantRepository = laborantRepository;
-        this.reportRepository = reportRepository;
+    public MainController(LaborantService laborantService, ReportService reportService) {
+        this.laborantService = laborantService;
+        this.reportService = reportService;
     }
 
     @GetMapping("/reports")
     public List<Report> getAllReports(){
-        return reportRepository.findAll();
+        return reportService.findAll();
     }
 
-    
+    @GetMapping("/reports/{id}")
+    public Report getReport(@PathVariable Integer id){
+
+        Report report = reportService.findById(id);
+
+        if(report == null){
+            throw new RuntimeException("Report not found with id: "+ id);
+        }
+        return report;
+    }
 
 
+    @PostMapping("/reports/{laborantHospitalIdNo}")
+    public Report createReport(@PathVariable String laborantHospitalIdNo, @RequestBody Report report){
+
+        Laborant currentLaborant = laborantService.findLaborantByHospitalIdNo(laborantHospitalIdNo);
+        currentLaborant.addReport(report);
+
+        return reportService.save(report);
+    }
 
 
 
