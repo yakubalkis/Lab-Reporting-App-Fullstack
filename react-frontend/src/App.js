af1aa58c217a2f059478a4253631230be5b4ed4e
yakubalkis <alkis.yakub@gmail.com>
2023-05-16 22:56:30 +0300

    added route for AddReport component.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import ListReports from './components/ListReports';
 import Registration from './components/Registration';
+import AddReport from './components/AddReport';
 
 function App() {
   return (
@@ -11,8 +12,11 @@ function App() {
         <Header/>
         <div className="container">
             <Routes>
+
                 <Route path="/" element={<Registration />} />
-                <Route path="/:laborantHospitalIdNo/reports" element={<ListReports />} />
+                <Route path="/laborant/:laborantHospitalIdNo/reports" element={<ListReports />} />
+                <Route path="/laborant/:laborantHospitalIdNo/:reportId" element={<AddReport />} />
+                
             </Routes>
         </div>
 
