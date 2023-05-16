ea1eb50da1e99c0cfec70aa9548219a9430327b7
yakubalkis <alkis.yakub@gmail.com>
2023-05-16 22:55:55 +0300

    added "add report" button dynamically.

--- react-frontend/src/components/ListReports.js
+++ react-frontend/src/components/ListReports.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
+import {Link, useParams} from "react-router-dom";
 import Report from "./Report";
 const LAB_API_BASE_URL = "http://localhost:8080/api/v1/reports";
 
 export default function ListReports(){
 
     const [reportList, setReportList] = useState([]);
+    const {laborantHospitalIdNo} = useParams();
     
     
 
@@ -17,14 +19,21 @@ export default function ListReports(){
             console.log(reportList)
     }, []);
  
-    const isThereReport = reportList.length > 0 ? true : false ;
-    const HeadContent = reportList.length > 0 ? <h4 style={{marginLeft: "-10px", marginTop: "5px"}}>There are no report. You should add a report.</h4> : <h3>There are no report. You should add a report.</h3> ; // conditional content
+    
+    const HeadContent = reportList.length > 0 ? <></> : <h4 style={{marginLeft: "-10px", marginTop: "5px"}}>There are no report. You should add a report.</h4> ; 
     const ReportsTable = reportList.length > 0 ? <Report reports={reportList} /> : <></> ;
 
     return(
         <div>
+
             {HeadContent}
+
+            <div className="row">
+                <Link to={`/laborant/${laborantHospitalIdNo}/${0}`}><button style={{marginTop: "1rem", marginBottom: "0.5rem"}} className="btn btn-primary" > Add Report </button></Link>
+            </div>
+
             {ReportsTable}
+
         </div>
 
     )
