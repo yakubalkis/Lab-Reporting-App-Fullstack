fa5ab4885ed191329a2f585b4b64bc3a2e78f6af
yakubalkis <alkis.yakub@gmail.com>
2023-05-17 21:33:12 +0300

    added func to convert byte array of image to image

--- react-frontend/src/components/Report.js
+++ react-frontend/src/components/Report.js
@@ -1,10 +1,14 @@
 
 import axios from "axios";
+import { useState } from "react";
 import {useParams,useNavigate} from "react-router-dom";
+import {Buffer} from 'buffer';
 const LAB_API_BASE_URL = "http://localhost:8080/api/v1/reports";
+const LAB_API_FILE_BASE_URL = "http://localhost:8080/api/v1/file";
 
 export default function(props){
-
+    
+    const [imgData, setImageData] = useState(null);
     const navigate = useNavigate();
     const {laborantHospitalIdNo} = useParams();
 
@@ -17,17 +21,24 @@ export default function(props){
         window.location.reload();
     }
 
-    function seeImage(id){
-        
-    }
+    function seeImage(id, imageName){
 
+        axios.get(LAB_API_FILE_BASE_URL +"/" +imageName, {
+            responseType: 'arraybuffer'})
+            .then((res) => {
+                setImageData(() => Buffer.from(res.data, 'binary').toString('base64'))
+            })
+    }
+    console.log(imgData)
+   // console.log(URL.createObjectURL(imgData))
     return(
         <div className="row">
+        <img src={`data:image/png;base64,`+imgData} />
         <table className="table table-striped table-bordered">
-
+            
             <thead>
                 <tr>
-                    <th>First Name</th>
+                    <th>First Name</th> 
                     <th>Last Name</th>
                     <th>TC No</th>
                     <th>Diagnosis Title</th>
@@ -49,7 +60,7 @@ export default function(props){
                             <td>{report.diagnosisDetail}</td>
                             <td>{report.date}</td>
                             <td>
-                                <button onClick={() => seeImage(report.id)} className="btn btn-info btn-dark">Click to See</button>
+                                <button onClick={() => seeImage(report.id, report.imageName)} className="btn btn-info btn-dark">Click to See</button>
                             </td>
                             <td>
                                 <button onClick={() => editReport(report.id)} className="btn btn-info">Update</button>
