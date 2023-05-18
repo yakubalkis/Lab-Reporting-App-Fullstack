59ae66faf4950b8f495e30f531ce9aef8afb292e
yakubalkis <alkis.yakub@gmail.com>
2023-05-18 18:14:28 +0300

    added filter functionality to page.

--- react-frontend/src/components/ListReports.js
+++ react-frontend/src/components/ListReports.js
@@ -9,6 +9,7 @@ const LAB_API_BASE_URL = "http://localhost:8080/api/v1";
 export default function ListReports(){
 
     const [reportList, setReportList] = useState([]);
+    const [filteredReportList, setFilteredReportList] = useState([]);
     const [filterType, setFilterType] = useState("Choose Filter type");
     const {laborantHospitalIdNo} = useParams();
     
@@ -18,17 +19,36 @@ export default function ListReports(){
         axios.get(LAB_API_BASE_URL+"/reports")
             .then((res) => {
                 setReportList(res.data)
+                setFilteredReportList(res.data) // at first, since there is no filter, filteredReportList has all data too
             })
     },[]);
 
     function handleChange(type){
         setFilterType(type)
-        console.log(type)
+    }
+
+    function handleSearchInput(e){
+        
+        const inputSearch = e.target.value;
+       
+        if(filterType==="By Patient Name/Surname"){
+            
+            setFilteredReportList(reportList.filter((report) => report.firstName.toLowerCase().includes(inputSearch.toLowerCase())))
+                if(filteredReportList.length===0){ // if not found by name, try by surname
+                    setFilteredReportList(reportList.filter((report) => report.lastName.toLowerCase().includes(inputSearch.toLowerCase())))
+                }
+        }
+        else if(filterType==="By TC No"){
+            setFilteredReportList(reportList.filter((report) => report.tcNo.includes(inputSearch)))
+        }
+        else{ // if there is no filter type, as default, filter by name
+            setFilteredReportList(reportList.filter((report) => report.firstName.toLowerCase().includes(inputSearch.toLowerCase())))
+        }
     }
     
     
     const HeadContent = reportList.length > 0 ? <></> : <h4 style={{marginLeft: "-10px", marginTop: "5px"}}>There are no report. You should add a report.</h4> ; 
-    const ReportsTable = reportList.length > 0 ? <Report reports={reportList}  /> : <></> ;
+    const ReportsTable = filteredReportList.length > 0 ? <Report reports={filteredReportList}  /> : <></> ;
 
     return(
         <div>
@@ -37,8 +57,8 @@ export default function ListReports(){
             <div className="div-add-filter">
 
                 <Link to={`/laborant/${laborantHospitalIdNo}/${0}`}><button style={{marginTop: "1rem", marginBottom: "0.5rem"}} className="btn btn-primary" > Add Report </button></Link>
-
-                <input type="text" className="form-control" />
+                
+                <input type="text" className="form-control" placeholder="Search..." onChange={handleSearchInput} />
 
                 <DropdownButton id="dropdown-basic-button" title={filterType} className="dropdown-filter">
                     <Dropdown.Item onClick={() => handleChange("By Patient Name/Surname")}>By Patient Name/Surname</Dropdown.Item>
