50aec0d09414205e69f9c796b684a2e2058a252d
yakubalkis <alkis.yakub@gmail.com>
2023-05-21 02:24:05 +0300

    updated for Logout component.

--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -1,13 +1,25 @@
-import "../style/Header.css"
+import { useEffect, useState } from "react"
+import Logout from "./Logout"
+
 
 export default function Header(){
 
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const hostIdNoOfCurrentUser = localStorage.getItem("hostIdNoOfCurrentUser");
+    
 
+    useEffect(() => {
+        if(hostIdNoOfCurrentUser){
+            setIsLoggedIn(true)
+        }
+    }, []);
+    
     return(
         <div>
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div className="navbar-brand">Laboratory Reporting App</div>
+                    <div style={{ marginLeft: "1rem"}} className="navbar-brand">Laboratory Reporting App</div>
+                    {isLoggedIn && <Logout/>}
                 </nav>
             </header>
         </div>
