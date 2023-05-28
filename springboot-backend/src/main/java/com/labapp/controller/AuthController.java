cb2409aba600c75977b556f9bd2cc9fd909a3c13
yakubalkis <alkis.yakub@gmail.com>
2023-05-28 23:52:08 +0300

    added cross origin annotation.

--- springboot-backend/src/main/java/com/labapp/controller/AuthController.java
+++ springboot-backend/src/main/java/com/labapp/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
