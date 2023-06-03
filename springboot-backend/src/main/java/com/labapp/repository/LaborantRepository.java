d0aef5d8a734140938b72bd585335075f3bc98c8
yakubalkis <alkis.yakub@gmail.com>
2023-06-03 13:18:04 +0300

    added custom query method for searching process.

--- springboot-backend/src/main/java/com/labapp/repository/LaborantRepository.java
+++ springboot-backend/src/main/java/com/labapp/repository/LaborantRepository.java
@@ -4,9 +4,14 @@ import com.labapp.entity.Laborant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface LaborantRepository extends JpaRepository<Laborant, Long> {
 
     @Query("SELECT lab FROM Laborant lab WHERE lab.hospitalIdNo = ?1")
     Laborant findLaborantByHospitalIdNo(String hospitalIdNo);
+
+    @Query("SELECT l FROM Laborant l WHERE l.firstName LIKE ?1% OR l.lastName LIKE ?2%")
+    List<Laborant> getLaborantByName(String firstName, String lastName);
 
 }
