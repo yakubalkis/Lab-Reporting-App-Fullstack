8b8a68760433be97bc83e93a2941ebbb81395c05
yakubalkis <alkis.yakub@gmail.com>
2023-06-03 11:17:05 +0300

    added two custom query methods for searching

--- springboot-backend/src/main/java/com/labapp/repository/ReportRepository.java
+++ springboot-backend/src/main/java/com/labapp/repository/ReportRepository.java
@@ -2,6 +2,16 @@ package com.labapp.repository;
 
 import com.labapp.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    @Query("SELECT r FROM Report r WHERE r.firstName LIKE ?1% OR r.lastName LIKE ?2%")
+    List<Report> getByPatientNameORSurname(String firstName, String lastName);
+
+    @Query("SELECT r FROM Report r WHERE r.tcNo LIKE ?1%")
+    List<Report> getByTcNo(String tcNo);
+
 }
